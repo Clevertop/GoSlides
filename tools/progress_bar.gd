2a35d1236d80a2cb6632a1724315b7263a473a9a
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-09-27 17:15:11 +1000

    manual merge

--- tools/progress_bar.gd
+++ tools/progress_bar.gd
@@ -1,10 +1,17 @@
 extends ProgressBar
 
 @export var speed : float = 2
+@export var target_value : float
+
+func _ready():
+	max_value = 60000
 
 func _process(delta: float) -> void:
-	value = lerp(value, 100.0, speed * delta)
+	value = lerp(value, target_value, speed * delta)
 
 func _on_visibility_changed() -> void:
 	if visible:
-		value = 0
+		$Timer.start(0)
+
+func _on_timer_timeout() -> void:
+	value = 0
